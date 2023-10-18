67b54585abe6ce5ef0fad4a0f0e855820f5a08e3
guilhermerubrae <guilhermerubrae@gmail.com>
2023-10-17 21:50:47 -0300

    var

--- JS/Aula00-RLogico/Aula02-Var/main.js
+++ JS/Aula00-RLogico/Aula02-Var/main.js
@@ -1,13 +1,53 @@
-var nomeDoPrimeiroAluno = 'Guillermo'
+// var
+var nomeDoPrimeiroAluno = 'Guillermo';
 
-console.log(nomeDoPrimeiroAluno)
+var nomeDoPrimeiroAluno = 'Guillermoo';
 
-let numeroDeAprovados = 5
+console.log(nomeDoPrimeiroAluno);
 
-console.log(numeroDeAprovados)
+//let
+let numeroDeAprovados = 5;
 
-console.clear
+numeroDeAprovados = 6;
 
-var nomeDoPrimeiroAluno = 'Guillermo'
+console.log(numeroDeAprovados);
 
-console.log(nomeDoPrimeiroAluno)
+let nomeAluno = 'Guillermo';
+
+nomeAluno = 'Guilherme';
+
+console.log(nomeAluno);
+
+// limpar
+console.clear();
+
+// const
+const nomeSiteWeb = 'G';
+
+console.log(nomeSiteWeb);
+
+//-------
+
+let x = 1;
+
+let y = 22;
+
+let soma = x / y; 
+
+console.log (soma);
+
+let texto1 = 'game';
+
+texto2 = ' of thrones \nthrones';
+
+let somaString = texto1 +texto2
+
+console.log (somaString);
+
+console.clear();
+
+var verdadeiro = true;
+var falso = false;
+let nulo = null;
+
+//conversão automática no js null para 0; string num com num na subtração subtrai; com soma ele concatena; texto com num NaN
---
+++ JS/Aula00-RLogico/Exercício/main.js
@@ -0,0 +1,8 @@
+let notaAluno2Semestre1;
+console.log('Qual sua primeira nota? ')
+
+
+let notaAluno2Semestre2;
+console.log('Qual sua segunda nota? ')
+
+let media =notaAluno2Semestre1 + notaAluno2Semestre2
