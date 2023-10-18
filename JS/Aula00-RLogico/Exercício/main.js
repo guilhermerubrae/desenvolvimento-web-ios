dccce3240c81d1870033464bfb85acb0a4e4c54e
guilhermerubrae <guilhermerubrae@gmail.com>
2023-10-17 21:53:42 -0300

    var

--- JS/Aula00-RLogico/Exercício/main.js
+++ JS/Aula00-RLogico/Exercício/main.js
@@ -1,8 +1,8 @@
-let notaAluno2Semestre1;
+let notaAluno2Semestre1 = 4;
 console.log('Qual sua primeira nota? ')
 
 
-let notaAluno2Semestre2;
+let notaAluno2Semestre2 = 6; 
 console.log('Qual sua segunda nota? ')
 
-let media =notaAluno2Semestre1 + notaAluno2Semestre2
+let media = notaAluno2Semestre1 + notaAluno2Semestre2 / 2
