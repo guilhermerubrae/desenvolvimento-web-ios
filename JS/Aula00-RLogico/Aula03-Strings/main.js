624a158f4bcad2754bb768ecfe626ce14b74c7f8
guilhermerubrae <guilhermerubrae@gmail.com>
2023-10-19 21:23:12 -0300

    strings

--- JS/Aula00-RLogico/Aula03-Strings/main.js
+++ JS/Aula00-RLogico/Aula03-Strings/main.js
@@ -5,9 +5,14 @@ console.log(serie1 + " " + serie2);
 
 // templete string
 console.log(`${serie1} + texto aleatório que contabiliza espaço ${+ serie2}`);
-console.log(`${num1 + num2}`); //numericos
+// console.log(`${num1 + num2}`); //numericos
 
 console.clear();
 
-const = string01 = 'Instituto da Oportunidade';
-console.log(charAt(10)); 
+const string01 = 'Instituto da Oportunidade';
+console.log('Exemplo: ' + string01.charAt(10));
+console.log('Exemplo: ' + string01.length);
+console.log('Exemplo: ' + string01.toUpperCase());
+console.log('Exemplo: ' + string01.toLowerCase());
+console.log('Exemplo: ' + string01.substring(10,25) + ' ' + string01.substring(0,9));
+console.log('Exemplo: ' + string01.substring(13))
