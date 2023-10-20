637a36b5ac897c19b1e41adab9b82df73a035226
guilhermerubrae <guilhermerubrae@gmail.com>
2023-10-20 00:59:11 -0300

    subindo

--- JS/Aula02-Var/Exercício/main.js
+++ JS/Aula02-Var/Exercício/main.js
@@ -1,8 +1,8 @@
-let notaAluno2Semestre1 = 4;
-console.log('Qual sua primeira nota? ')
+let nota1 = 8;
+let nota2 = 6;
+let nota3 = 5;
+let nota4 = 10;
 
+let media = (nota1 + nota2 + nota3 + nota4) / 4;
 
-let notaAluno2Semestre2 = 6; 
-console.log('Qual sua segunda nota? ')
-
-let media = notaAluno2Semestre1 + notaAluno2Semestre2 / 2
+console.log("A média do aluno com as 4 notas é: " + media);
---
+++ JS/Aula03-Strings/Exercício/main.js
@@ -0,0 +1,6 @@
+let devsFuture = 'Estamos aprendendo JavaScript e seremos futuros Devs';
+console.log('Exemplo: ' + devsFuture.charAt(19));
+console.log('Exemplo: ' + devsFuture.length);
+console.log('Exemplo: ' + devsFuture.toUpperCase());
+console.log('Exemplo: ' + devsFuture.substring(48, 52));
+console.log('Exemplo: ' + devsFuture.substring(48));
