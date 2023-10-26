4ee22c21b12b53c2711f6d32e77f9b89eb5efe5b
guilhermerubrae <guilhermerubrae@gmail.com>
2023-10-26 15:49:48 -0300

    subindo

--- JS/Aula05-ERepetições/Exercício/main.js
+++ JS/Aula05-ERepetições/Exercício/main.js
@@ -1,8 +1,19 @@
-let notaAluno2Semestre1 = 4;
-console.log('Qual sua primeira nota? ')
+for(let tabuada = 1; tabuada <= 10; tabuada++){
+    console.log(`Tabuada do ${tabuada}`)
+    for(let contador = 1; contador <= 10; contador++){
+        console.log(`${tabuada} x ${contador} = ${tabuada * contador}`)
+}}
 
+console.clear()
 
-let notaAluno2Semestre2 = 6; 
-console.log('Qual sua segunda nota? ')
+let tabuada = 1
+while(tabuada <= 10){
+    console.log(`Tabuada do ${tabuada}`)
+    let contador = 1
+    while(contador <= 10){
+        console.log(`${tabuada} x ${contador} = ${tabuada * contador}`)
+        contador ++;
+    }
+    tabuada++;
+}      
 
-let media = notaAluno2Semestre1 + notaAluno2Semestre2 / 2
