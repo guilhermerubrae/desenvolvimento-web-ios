5620c1d62480611fef60755ae72e4ce15a4b1ed4
guilhermerubrae <guilhermerubrae@gmail.com>
2023-10-29 23:58:50 -0300

    subindo

--- JS/Aula06-Array/Exercício/main.js
+++ JS/Aula06-Array/Exercício/main.js
@@ -1,8 +1,43 @@
-let notaAluno2Semestre1 = 4;
-console.log('Qual sua primeira nota? ')
+//Array Unidimensional
+const arrayInformacoes = ["HTML", 1993, "CSS", 1996, "Bootstrap", 2011, "JS", 1995, "React", 2013, "Java", 1995];
 
+console.log("Array completo:", arrayInformacoes);
+console.log("Tamanho do Array:", arrayInformacoes.length);
 
-let notaAluno2Semestre2 = 6; 
-console.log('Qual sua segunda nota? ')
+const informacaoJS = arrayInformacoes[arrayInformacoes.indexOf("JS")];
+console.log("Informação 'JS':", informacaoJS);
 
-let media = notaAluno2Semestre1 + notaAluno2Semestre2 / 2
+const arrayAsString = arrayInformacoes.join(",");
+console.log("Tipo da String resultante:", typeof arrayAsString);
+
+const arrayAsStringComSeparadorModificado = arrayAsString.replace(/,/g, "/");
+console.log("Array com separador modificado:", arrayAsStringComSeparadorModificado);
+
+arrayInformacoes.unshift("IOS");
+console.log("Novo Array com 'IOS' no início:", arrayInformacoes);
+console.log("Tamanho do Array atualizado:", arrayInformacoes.length);
+
+//Array Bidimensional
+
+const arrayBidimensional = [
+    ["HTML", 1993, "CSS", 1996],
+    ["Bootstrap", 2011, "JS", 1995],
+    ["React", 2013, "Java", 1995]
+  ];
+  
+  console.log("Array completo:", arrayBidimensional);
+  console.log("Número de linhas do Array:", arrayBidimensional.length);
+  
+  const ano2013 = arrayBidimensional[2][1];
+  console.log("Ano 2013:", ano2013);
+  
+  arrayBidimensional[1][2] = "JavaScript";
+  console.log("Array com 'JavaScript':", arrayBidimensional);
+  
+  arrayBidimensional[2].splice(2, 2);
+  console.log("Array com 'Java' removido:", arrayBidimensional);
+  
+  arrayBidimensional.push(["pipoca"]);
+  console.log("Array com 'pipoca' no final:", arrayBidimensional);
+  
+
