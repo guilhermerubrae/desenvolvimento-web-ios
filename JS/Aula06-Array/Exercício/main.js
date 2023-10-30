b940b822e8efa7165273fb638ed7055b64ffd7cb
guilhermerubrae <guilhermerubrae@gmail.com>
2023-10-30 00:33:44 -0300

    desculpa o atraso

--- JS/Aula06-Array/Exercício/main.js
+++ JS/Aula06-Array/Exercício/main.js
@@ -1,17 +1,18 @@
 //Array Unidimensional
-const arrayInformacoes = ["HTML", 1993, "CSS", 1996, "Bootstrap", 2011, "JS", 1995, "React", 2013, "Java", 1995];
+
+let arrayInformacoes = ["HTML", 1993, "CSS", 1996, "Bootstrap", 2011, "JS", 1995, "React", 2013, "Java", 1995];
 
 console.log("Array completo:", arrayInformacoes);
 console.log("Tamanho do Array:", arrayInformacoes.length);
 
-const informacaoJS = arrayInformacoes[arrayInformacoes.indexOf("JS")];
-console.log("Informação 'JS':", informacaoJS);
+let informacaoJS = arrayInformacoes;
+console.log(informacaoJS[6]);
 
-const arrayAsString = arrayInformacoes.join(",");
-console.log("Tipo da String resultante:", typeof arrayAsString);
+let arrayString = arrayInformacoes.toString();
+console.log(typeof arrayString);
 
-const arrayAsStringComSeparadorModificado = arrayAsString.replace(/,/g, "/");
-console.log("Array com separador modificado:", arrayAsStringComSeparadorModificado);
+let arrayJoin = arrayInformacoes.join(' / ');
+console.log("Array com separador modificado:", arrayJoin);
 
 arrayInformacoes.unshift("IOS");
 console.log("Novo Array com 'IOS' no início:", arrayInformacoes);
@@ -19,7 +20,7 @@ console.log("Tamanho do Array atualizado:", arrayInformacoes.length);
 
 //Array Bidimensional
 
-const arrayBidimensional = [
+let arrayBidimensional = [
     ["HTML", 1993, "CSS", 1996],
     ["Bootstrap", 2011, "JS", 1995],
     ["React", 2013, "Java", 1995]
@@ -28,16 +29,16 @@ const arrayBidimensional = [
   console.log("Array completo:", arrayBidimensional);
   console.log("Número de linhas do Array:", arrayBidimensional.length);
   
-  const ano2013 = arrayBidimensional[2][1];
+  let ano2013 = arrayBidimensional[2][1];
   console.log("Ano 2013:", ano2013);
   
   arrayBidimensional[1][2] = "JavaScript";
-  console.log("Array com 'JavaScript':", arrayBidimensional);
+  console.log(arrayBidimensional);
   
-  arrayBidimensional[2].splice(2, 2);
-  console.log("Array com 'Java' removido:", arrayBidimensional);
+  delete arrayBidimensional[2][2];
+console.log(arrayBidimensional);
   
   arrayBidimensional.push(["pipoca"]);
-  console.log("Array com 'pipoca' no final:", arrayBidimensional);
+  console.log(typeof arrayBidimensional);
   
 
