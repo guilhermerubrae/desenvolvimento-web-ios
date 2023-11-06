8e5665d2f5e2183318c03b3963cced015ba9a25b
guilhermerubrae <guilhermerubrae@gmail.com>
2023-11-05 23:56:34 -0300

    subindo

---
+++ JS/Aula08-Funções/Exercício/main.js
@@ -0,0 +1,28 @@
+// Função que exibe um alerta de cookie
+function alertCookie() {
+    console.log('Você quer um cookie');
+}
+
+// Função de alerta de sucesso usando arrow function
+const alertSucesso = () => {
+    console.log('Mensagem gerada com sucesso');
+}
+
+// Objeto que descreve um notebook
+const notebook = {
+    marca: 'Samsung XR-T',
+    cor: 'Rosa',
+    peso: '2kg',
+    memoriaRam: '32gb',
+    SistemaOperacional: 'Linux'
+};
+console.log(notebook);
+
+// Função que multiplica dois valores
+function myFunc(valor1, valor2) {
+    return valor1 * valor2;
+}
+
+// Chamando a função e exibindo o resultado
+let conversao = myFunc(3, 365);
+console.log(conversao);
