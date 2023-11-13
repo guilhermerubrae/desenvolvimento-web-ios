67ba88a0b7334947d9d663eb68d0717e5ec066f7
guilhermerubrae <guilhermerubrae@gmail.com>
2023-11-13 00:08:11 -0300

    subindo

--- JS/Aula11-DOM-PT3/Exercício/main.js
+++ JS/Aula11-DOM-PT3/Exercício/main.js
@@ -12,3 +12,38 @@ botao.addEventListener("click", () => {
     imagem.alt = "The Umbrella Academy";
     document.body.appendChild(imagem);
 });
+
+
+  
+  function solicitarNome() {
+    // prompt
+    const nome = prompt("Digite seu nome:");
+  
+    //  usuário clicou em OK
+    if (nome !== null) {
+      const mensagem = document.createElement("p");
+      mensagem.textContent = `Olá ${nome}, bem-vindo à nossa academia`;
+  
+      // Inserção da mensagem
+      document.body.appendChild(mensagem);
+    }
+  }
+  
+  function exibirTabuada() {
+    // Solicitação do número via prompt
+    const numero = prompt("Digite um número para ver a tabuada:");
+  
+    // Verifica se o usuário clicou em OK e exibe a tabuada
+    if (numero !== null) {
+      const resultado = document.createElement("p");
+      resultado.textContent = `Tabuada do ${numero}:\n`;
+  
+      for (let i = 1; i <= 10; i++) {
+        const linha = `${numero} x ${i} = ${numero * i}\n`;
+        resultado.textContent += linha;
+      }
+  
+      // Insere a tabuada abaixo do botão
+      document.body.appendChild(resultado);
+    }
+  }
