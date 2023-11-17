4454ca6f5af21f41db3cf7942fb7c23866161e65
guilhermerubrae <guilhermerubrae@gmail.com>
2023-11-17 20:54:05 -0300

    subindo

--- REACT/primeiro-site-web/src/App.js
+++ REACT/primeiro-site-web/src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
 
-function App() {
+function Principal() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main>
+      <p style={{ fontSize: '2rem', color: 'purple', backgroundColor: 'orange' }}>
+        Estou participando do curso de IOS com o objetivo de adquirir conhecimentos fundamentais na área de desenvolvimento front-end. Meu intuito é aprender a colaborar eficientemente em projetos de equipe, aprimorar minhas habilidades de comunicação e, a longo prazo, evoluir como desenvolvedor.
+      </p>
+      <ul>
+        <li>Filme 1</li>
+        <li>Filme 2</li>
+        <li>Filme 3</li>
+        <li>Filme 4</li>
+        <li>Filme 5</li>
+      </ul>
+      <img src="url_do_seu_filme_favorito" alt="Filme favorito" />
+    </main>
   );
 }
 
-export default App;
+export default Principal;
