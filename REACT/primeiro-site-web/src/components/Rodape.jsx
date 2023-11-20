0f8834619a04fad5b3193b54bd70eafecce6befc
Guilherme Abreu <143297561+guilhermerubrae@users.noreply.github.com>
2023-11-20 00:53:40 -0300

    Update Rodape.jsx

--- REACT/primeiro-site-web/src/components/Rodape.jsx
+++ REACT/primeiro-site-web/src/components/Rodape.jsx
@@ -1,1 +1,13 @@
+import React from "react";
 
+const footer = () => {
+    return (
+        <>
+            <footer>
+                Guilherme Abreu do Nascimento &copy; 2023
+            </footer>
+        </>
+    );
+}
+
+export default footer;
